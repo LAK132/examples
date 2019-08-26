a869c056d0d856e65dc656fc97e7679b243b95b7
LAK132 <lakridesagain@gmail.com>
2019-08-26 19:59:11 +0930

    Add basic_opengl_window_polygons

---
+++ basic_opengl_window_polygons/makelist.bat
@@ -0,0 +1,25 @@
+set BINDIR=bin
+set OBJDIR=obj
+set COMDIR=..\common
+set OUTPUT=BasicOpenGLWindowPolygons.exe
+
+set INCDIRS=%COMDIR%\include %COMDIR%\include\SDL
+set LIBDIR=%COMDIR%\lib
+set LIBS=SDL2main.lib SDL2.lib
+
+set CXX=cl
+set CXXFLAGS=/nologo /std:c++17 /D_CRT_SECURE_NO_WARNINGS /MD /EHsc
+
+if "%mode%"=="release" (
+  set COMPFLAGS=/DNDEBUG /bigobj /O2
+  set LINKFLAGS=/SUBSYSTEM:CONSOLE
+)
+
+if "%mode%"=="debug" (
+  set COMPFLAGS=/Zi /bigobj /Od
+  set LINKFLAGS=/SUBSYSTEM:CONSOLE /DEBUG
+)
+
+call %COMDIR%\makelist.bat
+
+set SOURCE=src\main.cpp %GL_GL3W_C% %LAK_OPENGL_SHADER_CPP% %LAK_OPENGL_STATE_CPP%
